917bee7f27c27e50000c02ddc6d2209e2c421b10
Gavin Cabbage <gavin.cabbage@capitalone.com>
2016-10-13 18:10:01 -0400

    ready to try first deploy

---
+++ app/js/index.test.js
@@ -0,0 +1,11 @@
+var assert = require('chai').assert;
+
+var index = require('./index.js');
+
+describe('index', function() {
+
+    it('must be defined', function() {
+        assert.isDefined(index);
+    });
+
+});
