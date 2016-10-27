ce61a3b34857becbdd4ca68986217204a2d56aeb
Gavin Cabbage <gavin.cabbage@capitalone.com>
2016-10-27 15:28:23 -0400

    first commit with firebase

--- app/js/index.js
+++ app/js/index.js
@@ -6,4 +6,16 @@
 
     console.log('hello from index.js');
 
+    // Initialize Firebase
+    var config = {
+        apiKey: "AIzaSyBvWg_JPft53Qn_-FaSpim-P9tNnjzJeus",
+        authDomain: "giap-80ec2.firebaseapp.com",
+        databaseURL: "https://giap-80ec2.firebaseio.com",
+        storageBucket: "giap-80ec2.appspot.com",
+        messagingSenderId: "873686227264"
+    };
+    firebase.initializeApp(config);
+
+    console.log('done initializing firebase');
+
 })();
--- app/js/index.test.js
+++ app/js/index.test.js
@@ -1,11 +1,22 @@
 var assert = require('chai').assert;
 
-var index = require('./index.js');
+var index;
 
 describe('index', function() {
+
+    before(function () {
+        global.firebase = mockFirebase();
+        index = require('./index.js');
+    });
 
     it('must be defined', function() {
         assert.isDefined(index);
     });
 
 });
+
+function mockFirebase() {
+    return {
+        initializeApp: function() {}
+    }
+}
